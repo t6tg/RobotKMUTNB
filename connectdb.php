fe8cef18527fa36545acec443210126cff86852b
Thanawat Gulati <33445861+thanawatgulati@users.noreply.github.com>
2018-12-04 17:35:35 +0700

    Update connectdb.php

--- connectdb.php
+++ connectdb.php
@@ -1,5 +1,5 @@
 <?php
-    $dbcon = new mysqli('localhost','root','root','rcbkmutnb');
+    $dbcon = new mysqli('localhost','root','james','rcbkmutnb');
     $dbcon->set_charset("utf8");
     if($dbcon->connect_errno){
         die("Conncetion Failed" .$dbcon->connect_error);
